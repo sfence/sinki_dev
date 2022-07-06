6a9a11665c84859772b0fe9f86e07f7160e6aa4c
SFENCE <sfence.software@gmail.com>
2022-07-06 10:46:13 +0200

    Add nodebox info.

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 
 
 if false then
-  -- template
+  -- template obj file
   minetest.register_node("sinki_dev:node_0", {
       description = "Testovaci node",
       paramtype = "light",
@@ -9,6 +9,30 @@ if false then
       drawtype = "mesh",
       -- sem dej spravne jmeno modelu
       mesh = "MODEL.obj",
+      -- sem dej spravne nazvy textur
+      tiles = {
+        "TEXTURA_0.png",
+        "TEXTURA_1.png",
+        "TEXTURA_2.png",
+        "TEXTURA_3.png",
+        "TEXTURA_4.png",
+        "TEXTURA_5.png",
+      },
+      groups = {oddly_breakable_by_hand = 2},
+    })
+  -- template node box
+  minetest.register_node("sinki_dev:node_0", {
+      description = "Testovaci node",
+      paramtype = "light",
+      paramtype2 = "facedir",
+      drawtype = "nodebox",
+      -- sem zkopiruj vygenerovany nodebox
+      nodebox = {
+        type = "fixed",
+        fixed = {
+          {-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
+        },
+      },
       -- sem dej spravne nazvy textur
       tiles = {
         "TEXTURA_0.png",
